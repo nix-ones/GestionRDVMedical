06bbe85d21364eeaf5923653cce2173f1510f506
nix-ones <beanixon@hotmail.fr>
2025-03-11 18:37:17 +0100

    first commit

---
+++ src/main/java/com/example/gestionmedicalrdv/GestionmedicalrdvApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestionmedicalrdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionmedicalrdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionmedicalrdvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestionmedicalrdv/GestionmedicalrdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionmedicalrdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionmedicalrdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
